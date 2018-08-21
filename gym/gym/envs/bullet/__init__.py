a112160b0be7927666a10befce15e2ed839f2c96
zhxx1987 <zhangshinshin@gmail.com>
2018-08-21 09:00:02 +0800

    add

---
+++ gym/gym/envs/bullet/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.bullet.env_bases import *
+from gym.envs.bullet.gym_locomotion_envs import *
+from gym.envs.bullet.robot_bases import *
+from gym.envs.bullet.robot_locomotors import *
+from gym.envs.bullet.scene_abstract import *
+from gym.envs.bullet.scene_stadium import *
